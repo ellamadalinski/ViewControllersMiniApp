67860cf5aa33ec2cb9da7e400f002111ecfb0ebd
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-23 20:32:45 -0500

    started mop

---
+++ ViewControllersMiniApp/Image1VC.swift
@@ -0,0 +1,12 @@
+//
+//  Image1VC.swift
+//  ViewControllersMiniApp
+//
+//  Created by Ella Madalinski on 9/23/21.
+//
+
+import UIKit
+
+class Image1VC: NSObject {
+
+}
---
+++ ViewControllersMiniApp/Image1VCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Image1VCViewController.swift
+//  ViewControllersMiniApp
+//
+//  Created by Ella Madalinski on 9/23/21.
+//
+
+import UIKit
+
+class Image1VCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
